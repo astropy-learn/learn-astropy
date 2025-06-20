3c9c8ed1b5feec5b4c2ba727666f98f32e20e384
Jeff Jennings <jjennings@flatironinstitute.org>
2025-06-20 14:05:35 -0400

    reach dir

--- deployment/installtutorials.py
+++ deployment/installtutorials.py
@@ -57,14 +57,12 @@ def process_repo(repo, destination_directory):
         print(f"Found tutorial(s) {tutorials}")
         for t in tutorials:
             shutil.copy(t, destination_directory)
-            shutil.copy(f"{repo}/{os.path.splitext(t)[0]}.html", destination_directory)
+            shutil.copy(f"{repo}/{os.path.basename(t)}.html", destination_directory)
         if len(tutorials) > 1:
             print("More than 1 tutorial found; treating this as a book")
             shutil.copy(f"{repo}/index.html", destination_directory)
         # copy images (plots) in notebook for faster page loading
-        shutil.copy(
-            f"{repo}/_images/*.png", os.path.join(destination_directory, "nboutput")
-        )
+        shutil.copy(f"{repo}/_images/*.png", f"{destination_directory}/nboutput")
 
 
 if __name__ == "__main__":
